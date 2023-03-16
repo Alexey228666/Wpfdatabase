aca4c4654f35d5916e818cf9ad686f9c06f9f8c3
Alexey228666 <116977230+Alexey228666@users.noreply.github.com>
2023-03-16 23:05:24 +0300

    Add files via upload

---
+++ Wpfdatabase/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Lab1DataSetTableAdapters;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PersonDepartmentTableAdapter person = new PersonDepartmentTableAdapter();
+        DepartmentTableAdapter department = new DepartmentTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            PersonDataGrid.ItemsSource = person.GetData();
+            DepartmentComboBox.ItemsSource = department.GetData();
+            DepartmentComboBox.DisplayMemberPath = "Title";
+        }
+
+        private void DepartmentComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show((DepartmentComboBox.SelectedItem as DataRowView).Row[1].ToString());
+        }
+    }
+}
